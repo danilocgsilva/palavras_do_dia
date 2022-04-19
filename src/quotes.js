c90884783f980715ef048413600ecb344f4f1819
Danilo Silva <danilocgsilva@gmail.com>
2022-04-19 12:19:51 -0300

    Adição de novas citações

---
+++ src/quotes.js
@@ -0,0 +1,429 @@
+let quotes = [
+    {
+        "quote": "Mais um dia para mudar o mundo..."
+    },
+    {
+        "quote": "Como é bom saber que é possível recomeçar tudo de novo.",
+        "author": "Inajá Martins de Almeida"
+    },
+    {
+        "quote": "O primeiro dos bens, depois da saúde, é a paz interior",
+        "author": "François de La Rochefoucauld"
+    },
+    {
+        "quote": "Com o conhecimento nossas dúvidas aumentam.",
+        "author": "Johann Goethe"
+    },
+    {
+        "quote": "As palavras são os suspiros da alma.",
+        "author": "Pitágoras"
+    },
+    {
+        "quote": "As pessoas felizes lembram o passado com gratidão, alegram-se com o presente e encaram o futuro sem medo.",
+        "author": "Epícuro"
+    },
+    {
+        "quote": "Todas as vidas de homens são contos de fadas escritos pelas mãos de Deus.",
+        "author": "Hans Christian Andersen"
+    },
+    {
+        "quote": "A felicidade é simplesmente uma questão de luz interior.",
+        "author": "Henri Lacordaire"
+    },
+    {
+        "quote": "Mesmo as noites totalmente sem estrelas podem anunciar a aurora de uma grande realização.",
+        "author": "Martin Luther King"
+    },
+    {
+        "quote": "Todo homem tem o direito de decidir o seu próprio destino.",
+        "author": "Bob Marley"
+    },
+    {
+        "quote": "Apenas um raio de sol é suficiente para afastar várias sombras.",
+        "author": "São Francisco de Assis."
+    },
+    {
+        "quote": "Cultivar estados mentais positivos, como a generosidade e a compaixão, leva à felicidade.",
+        "author": "Dalai lama"
+    },
+    {
+        "quote": "O homem é um gênio quando está sonhando.",
+        "author": "Akira Kurosawa"
+    },
+    {
+        "quote": "Nenhuma atividade no bem é insignificante. As mais altas árvores são oriunidas de minúsculas sementes.",
+        "author": "Chico Xavier."
+    },
+    {
+        "quote": "O bom humor é a única qualidade divina do homem.",
+        "author": "Arthur Schopenhauer"
+    },
+    {
+        "quote": "Alimente seu cérebro com pensamentos saudáveis, para que seu corpo possa refletir saúde.",
+        "author": "Minutos de Sabedoria"
+    },
+    {
+        "quote": "Abra seus braços para as mudanças, mas não abra mão de seus valores.",
+        "author": "Dalai Lama"
+    },
+    {
+        "quote": "Um grande homem sem religião é um simples animal sem alma.",
+        "author": "Daniel Defoe"
+    },
+    {
+        "quote": "A cidadania não é atitude passiva, mas ação permanente, em favor da comunidade.",
+        "author": "Tancredo Neves"
+    },
+    {
+        "quote": "Outrora, a velhice era uma dignidade; hoje, ela é um peso.",
+        "author": "Fraçois René de Chateaubriand"
+    },
+    {
+        "quote": "Seus sonhos são as letras do livro que sua vida está escrevendo.",
+        "author": "Paulo Coelho"
+    },
+    {
+        "quote": "A gula mata mais do que a espada.",
+        "author": "George Herbert"
+    },
+    {
+        "quote": "Uma boa cabeça e um bom coração são sempre uma formidável combinação",
+        "author": "Nelson Mandela"
+    },
+    {
+        "quote": "Eu não destruo um inimigo quando eu o torno meu amigo?",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "quote": "... se você quiser testar o caráter de um homen, dê-lhe poder.",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "quote": "Seu eu tivesse 9 horas para cortar uma árvore, passaria 6 horas afiando meu machado",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "quote": "Invenção requer duas coisa. 1. A habilidade de tentar muitos experimentos. 2. Não ter que viver com danos colaterais de experimentos falhos.",
+        "author": "Andy Jassy"
+    },
+    { 
+        "quote": "Talentoso faz o que pode; gênio faz o que é preciso.",
+        "author": "Edward Bulwer Lytton"
+    },
+    {
+        "quote": "Devemos prestar atenção à qualidade das ações que praticamos.",
+        "author": "Aristóteles"
+    },
+    {
+        "quote": "O sonho e a esperança são dois calmantes que a Natureza concede ao ser humano.",
+        "author": "Frederico I"
+    },
+    {
+        "quote": "Dinheiro é um negócio curioso. Quem não tem está louco para ter, quem tem está cheio de problemas por causa dele.",
+        "author": "Ayrton Senna"
+    },
+    {
+        "quote": "Não há nada melhor como um sonho para criar o futuro.",
+        "author": "Victor Hugo"
+    },
+    { 
+        "quote": "As paixões cegam. O verdadeiro amor nos torna lúcidos.",
+        "author": "Desconhecido"
+    },
+    { 
+        "quote": "Não é preciso experiência mística para descobrir que o mundo é bom.",
+        "author": "Chogyam Trunpga"
+    },
+    {
+        "quote": "Os fatos não deixam de existir só porque sãi ignorados.",
+        "author": "Audous Huxley"
+    },
+    {
+        "quote": "A beleza das coisas existe no espírito dem quem as contempla",
+        "author": "David Hume"
+    },
+    {
+        "quote": "O otimismo é a fé em ação.",
+        "author": "Helen Keller"
+    },
+    {
+        "quote": "Grandes resultados requerem grandes ambições.",
+        "author": "Heráclito"
+    },
+    {
+        "quote": "A paz mais desvantajosa é melhor do que a guerra mais justa.",
+        "author": "Erasmo de Rotterdam"
+    },
+    {
+        "quote": "É preciso não relaxar nunca, mesmo tendo chegado tao longe.",
+        "author": "Paulo Coelho"
+    },
+    {
+        "quote": "A Terra é minha casa e a humanidade, a minha família.",
+        "author": "Khalil Gibran"
+    },
+    {
+        "quote": "A verdade só é encontrada quando os homens são livres para a perseguir.",
+        "author": "Franklin Delano Roosevelt"
+    },
+    {
+        "quote": "A palavra foi dada ao homem para disfarçar o pensamento.",
+        "author": "Charles Talleyrand-Périgord"
+    },
+    {
+        "quote": "A felicidade é um bem que se multiplica ao ser dividida.",
+        "author": "Marxwell Maltz"
+    },
+    { 
+        "quote": "A vida é como um livro que deve ser folheado página por página sem se consultar o índice.",
+        "author": "Desconhecido"
+    },
+    {
+        "quote": "Não acrescente dias à sua vida, mas vida aos seus dias.",
+        "author": "Harry Benjamin"
+    },
+    {
+        "quote": "Uma longa viagem começa com um único passo.",
+        "author": "Lao-Tsé"
+    },
+    {
+        "quote": "A Natureza é o único livro que oferece um conteúdo valioso em todas as suas folhas.",
+        "author": "Johann Goethe"
+    },
+    {
+        "quote": "Depois da tempestade vem a bonança.",
+        "author": "Desconhecido"
+    },
+    {
+        "quote": "Quando Deus quer, não há quem não queira.",
+        "author": "Ayrton Senna"
+    },
+    {
+        "quote": "Os investimentos em conhecimento geram os melhores dividendos.",
+        "author": "Benjamin Franklin"
+    },
+    {
+        "quote": "Nem o sacerdote nem o soldado devem sentir as inquietações de dúvida.",
+        "author": "Anatole France"
+    },
+    { 
+        "quote": "As mais preciosas qualidades da mulher não estão na inteligência; estão no instinto.",
+        "quote": "Samuel Smiles"
+    },
+    {
+        "quote": "Acreditar é o segredo para aqueles que querem transformar sonhos em realidade.",
+        "author": "David C. Haubert"
+    },
+    { 
+        "quote": "Pegamos o telefone que o menino fez com duas caixas de papelão e pedimos uma ligação com a infância.",
+        "author": "Millôr Fernandes"
+    },
+    { 
+        "quote": "O que faz o homem feliz não é o dinheiro: são a retidão e a prudência.",
+        "author": "Demócrito"
+    },
+    { 
+        "quote": "Cultura é o que fica depois de se esquecer tudo o que foi aprendido.",
+        "author": "André Malrax"
+    },
+    { 
+        "quote": "Todo homem é um poeta quando está apaixonado.",
+        "author": "Platão"
+    },
+    {
+        "quote": "O consumo é a única finalidade e o único propósito de toda produção.",
+        "author": "Adam Smith"
+    },
+    {
+        "quote": "As esperanças são como as estrelas: brilham, mas não trazem luz; lindas, mas ninguém as alcança.",
+        "author": "Coelho Neto"
+    },
+    {
+        "quote": "É durante as tempestades que o verdadeiro marinheiro aprende a velejar.",
+        "author": "Ricardo Jordão Magalhães"
+    },
+    { 
+        "quote": "Somente quando encontramos o amor é que descobrimos o que nos faltava na vida.",
+        "author": "John Ruskin"
+    },
+    { 
+        "quote": "Feliz aquele que transfere o que sabe e aprende o que ensina.",
+        "author": "Cora Coralina"
+    },
+    {
+        "quote": "O teatro é o primeiro soro que o homem inventou para se proteger da doença da angústia.",
+        "author": "Jean Barrault"
+    },
+    {
+        "quote": "Só percebemos o valor da água depois que a fonte seca",
+        "author": "Provérbio Popular"
+    },
+    {
+        "quote": "Devemos esperar o melhor, mas estar preparado para o pior.",
+        "author": "Desconhecido"
+    },{
+        "quote": "Como fica forte uma pessoa quando está segura de ser amada!",
+        "author": "Sigmund Freud"
+    },{
+        "quote": "Que o homem possa dedicar seus dias às atividades para as quais possui melhor habilidade.",
+        "author": "Sextus Propertius"
+    }, {
+        "quote": "O sinal mais evidente da sabedoria é o constante bom humor.",
+        "author": "Michel de Montaigne"
+    }, { 
+        "quote": "Quem decide pode errar; quem não decide já errou.",
+        "author": "Herbert von Karajan"
+    }, {
+        "quote": "Todo homem é útil à humanidade pelo simples fato de existir.",
+        "author": "Jean-Jacques Rousseau"
+    },
+    {
+        "quote": "A juventude feliz porque tem a capacidade de ver a beleza.",
+        "author": "Franz Kafka"
+    },
+    {
+        "quote": "O presente é a sombra que se move separando o ontem do Amanhã.  nela repousa a esperança.",
+        "author": "Frank Lloyd Wrigth"
+    },
+    {
+        "quote": "A mentira é o único privilégio do Homem Sobre todos os outros animais.",
+        "author": "Fiódor Dosdoiévski"
+    },
+    {
+        "quote": "O escritor é um homem que mais do que qualquer outro tem dificuldade para escrever",
+        "author": "Thomas Mann"
+    },
+    {
+        "quote": "Evite desencorajar-se: mantenha ocupações e faça do otimismo a maneira de viver. Isso Restaura a fé em si.",
+        "author": "Lucille Ball"
+    },
+    {
+        "quote": "Essencial faz a vida valer a pena.",
+        "author": "Charles Chaplin"
+    },
+    {
+        "quote": "Todas as leis humanas se alimentam da lei divina.",
+        "author": "Heráclito"
+    },
+    {
+        "quote": "Sobre a terra, antes da escrita da impressão, existiu a poesia.",
+        "author": "Pablo Neruda"
+    },
+    {
+        "quote": "Um verdadeiro amigo é aquele que entra quando o resto do mundo sai.",
+        "author": "Walter Winchell"
+    },
+    {
+        "quote": "A educação tem raízes amargas, mas os seus frutos são doces.",
+        "author": "Aristóteles"
+    },
+    {
+        "quote": "Querer ser bem sucedido sem trabalhar duro e como querer colher sem plantar.",
+        "author": "David Bly"
+    },
+    {
+        "quote": "A vida começa todos os dias.",
+        "author": "Érico Veríssimo"
+    },
+    {
+        "quote": "As iniciativas da Juventude valem tanto quanto as experiências dos mais velhos.",
+        "author": "Josephine von Knorr"
+    },
+    {
+        "quote": "A ambição universal dos homens é viver colhendo o que nunca plantaram.",
+        "author": "Adam Smith"
+    },
+    {
+        "quote": "Não podemos fazer tudo imediatamente, mas podemos fazer alguma coisa já.",
+        "author": "Calvin Coolidge"
+    },
+    {
+        "quote": "O prazer de uma boa ação é a única trazer se mistura de dor.",
+        "author": "Camilo Castelo Branco"
+    },
+    {
+        "quote": "Lembre-se que as pessoas podem tirar tudo de você, menos o seu conhecimento.",
+        "author": "Philips Brooks"
+    },
+    {
+        "quote": "Otimismo é esperar pelo melhor. Confiança é saber lidar com o pior.",
+        "author": "Roberto Simonsen"
+    },
+    {
+        "quote": "A coragem é a primeira das qualidades humanas porque garante todas as outras.",
+        "author": "Aristóteles"
+    },
+    {
+        "quote": "A terra provê o suficiente para as necessidades de todos os homens, mas não para voracidade de todos.",
+        "author": "Mahatma Gandhi"
+    },
+    {
+        "quote": "Imaginação cria formas, inteligência organiza o conteúdo.",
+        "author": "Elvio Antunes de Arruda"
+    },
+    {
+        "quote": "Deus não escuta a voz, mas sim o coração.",
+        "author": "Clarice Lispector"
+    }, {
+        "quote": "A pureza de coração é inseparável da simplicidade e da humanidade.",
+        "author": "Allan Kardec"
+    }, {
+        "quote": "Educação nunca foi despesa. Sempre foi investimento com retorno garantido.",
+        "author": "Arthur Lewis"
+    }, {
+        "quote": "Amigos, peçam alegria à Deus. Sejam alegres como as crianças e como os pássaros do céu!",
+        "author": "Fiódor Dostoiévski"
+    }, {
+        "quote": "Um sorriso é a mais bela maquiagem que uma mulher pode usar em seu rosto.",
+        "author": "Rafael Laimer Bilibio"
+    }, {
+        "quote": "O valor dos grandes homens mede-se pela importância dos serviços prestados à humanidade.",
+        "author": "Voltaire"
+    }, {
+        "quote": "A necessidade de procurar a verdadeira felicidade é o fundamento da nossa liberdade.",
+        "author": "John Locke"
+    }, {
+        "quote": "A cortesia é irmã da caridade, que apaga o ódio e fomenta o amor.",
+        "author": "São Francisco de Assis"
+    }, {
+        "quote": "A arte é um resumo da natureza feito pela imaginação.",
+        "author": "Eça de Queiroz"
+    }, {
+        "quote": "É comprida a estrela que vai desde a intenção até a execução.",
+        "author": "Jean Molière"
+    }, {
+        "quote": "O silêncio é um dos argumentos mais difíceis de ser rebater.",
+        "author": "Josh Bilings"
+    }, {
+        "quote": "Todos pensam em mudar o mundo, mas ninguém pensa em mudar a si mesmo.",
+        "author": "Leon Tolstói"
+    }, {
+        "quote": "Só é útil o conhecimento que nos torna melhores.",
+        "author": "Sócrates"
+    }, {
+        "quote": "Há lágrimas que correm pela Face e outras que rolam pelo coração.",
+        "author": "Desconhecido"
+    }, {
+        "quote": "Precisamos manter a vida com folga, para que a urgência não se faz necessária.",
+        "author": "Walter Grando"
+    }, {
+        "quote": "A beleza ideal está na simplicidade calma e serena.",
+        "author": "Johann Goethe"
+    }, {
+        "quote": "O mais importante na comunicação é escutar aquilo que não foi dito.",
+        "author": "Peter Drucker"
+    }, {
+        "quote": "A violência destrói o que ela pretende defender: a dignidade da vida, a liberdade do ser humano.",
+        "author": "Papa João Paulo II"
+    }, {
+        "quote": "Duas coisas instruem um homem, Qualquer que seja a sua natureza: o instinto e a experiência.",
+        "author": "Blaise Pascal"
+    }, {
+        "quote": "Para os males extremos só são eficazes remédios intensos.",
+        "author": "Hipócrates"
+    }, {
+        "quote": "Como a abelha trabalha na escuridão, o pensamento trabalha no silêncio e a virtude, no segredo.",
+        "author": "Mark Twain"
+    }
+]
+export default quotes
