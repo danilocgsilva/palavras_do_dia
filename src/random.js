8b1e6f72dd969ca193f3f988e90980a22363d0a8
Danilo Silva <danilocgsilva@gmail.com>
2022-04-24 22:25:46 -0300

    Lógica de criação de link em função exclusiva

--- src/random.js
+++ src/random.js
@@ -4,7 +4,7 @@ function random() {
     let quote = quotes[randomPos()];
     let text = `"${quote.quote}"`;
     if (quote.author) {
-        text += quote.author !== "" ? ` <a href="https://pt.wikipedia.org/wiki/${quote.author}" target="_blank">${quote.author}</a>` : "author desconhecido";
+        text += makeLink(quote)
     }
     return text;
 }
@@ -13,6 +13,10 @@ function randomPos() {
     return Math.floor(
         Math.random() * quotes.length
     )
+}
+
+function makeLink(quote) {
+    return quote.author !== "" ? ` <a href="https://pt.wikipedia.org/wiki/${quote.author}" target="_blank">${quote.author}</a>` : "autor desconhecido";
 }
 
 export default random
